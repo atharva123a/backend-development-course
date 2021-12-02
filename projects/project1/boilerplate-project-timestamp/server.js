c8a08855054778f194261f198d6815cfff361f09
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-12-02 11:46:56 +0530

    initial commit
    This is functional for valid input

---
+++ projects/project1/boilerplate-project-timestamp/server.js
@@ -0,0 +1,52 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date", function(req, res) {
+  const time = (req.params.date);
+
+  const arr = time.split('-');
+  let unix = "", utc = "";
+
+  if (arr.length > 1) {
+    // conver the utc to unix:
+    let a = arr.join('.')
+    unix = parseInt(new Date(a).getTime());
+  }
+  else {
+    unix = parseInt(time)
+  }
+  let d = new Date(unix).toString();
+  d = d.split(' ');
+  utc += d[0] + ", " + d[2] + " " + d[1] + " " + d[3] + " " + d[4] + " " + "GMT";
+
+
+  res.json({
+    "unix": unix, "utc": utc
+  })
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
