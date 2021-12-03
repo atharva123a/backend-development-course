4d29dc0ab417b8e27c95bf62fe382a26972a5d80
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-12-03 13:42:29 +0530

    return ip address as json object

--- projects/project2/boilerplate-project-headerparser/server.js
+++ projects/project2/boilerplate-project-headerparser/server.js
@@ -20,10 +20,15 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// route for returning json:
+app.get('/api/whoami', (req, res)=>{
+  // gets and stores the ip address of the device the website is accessed from:
+  const ip = req.headers['x-forwarded-for'];
+
+  res.json({
+    "ip" : ip
+  })
+})
 
 
 
