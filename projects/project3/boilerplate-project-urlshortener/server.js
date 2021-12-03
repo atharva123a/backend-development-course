d2676a2203b72e67909ac87cfebf8a3cab062c17
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-12-03 23:58:33 +0530

    server.js can store new urls and ids

--- projects/project3/boilerplate-project-urlshortener/server.js
+++ projects/project3/boilerplate-project-urlshortener/server.js
@@ -1,10 +1,18 @@
+
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+
+// this would store all urls objects:
+const urls = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
@@ -14,10 +22,38 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+// Your API for handling post requests:
+app.post('/api/shorturl', (req, res) => {
+  // let val = req.body.get['url_input']
+  const original_url = req.body.url;
+
+  const { length } = urls;
+  let short_url = length + 1;
+
+  const found = urls.some(el => el.original_url == original_url);
+
+  if (!found) {
+    urls.push({
+      'original_url': original_url,
+      "short_url": short_url
+    })
+  }
+  else {
+    let indx = urls.findIndex((url) => {
+      return url.original_url == original_url
+    });
+    short_url = urls[indx].short_url;
+  }
+
+  res.json({
+    "original_url": original_url,
+    "short_url": short_url
+  })
+
+})
+
+// API for handling get requests:
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
