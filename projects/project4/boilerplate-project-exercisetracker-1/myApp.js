588b1f5d91dd03b47d4ccee2f9290275a12ceb72
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-12-04 22:43:23 +0530

    myApp.js now has more versatile functionality

--- projects/project4/boilerplate-project-exercisetracker-1/myApp.js
+++ projects/project4/boilerplate-project-exercisetracker-1/myApp.js
@@ -4,28 +4,54 @@ const { Schema } = mongoose;
 const myURI = process.env['MONGO_URI']
 
 mongoose.connect(myURI, {
+  // this tells mongoose to drop support for the old url parser:
   useNewUrlParser: true,
+
+  // makes use of the mongodb new connection management engine:
   useUnifiedTopology: true
 });
 
-
+// define userSchema:
 let userSchema = new Schema({
   username : String
 })
 
+// make use of this schema to create User object
 let User = mongoose.model("User", userSchema);
 
+
+// function to create and save url;
+// we made use of it to handle post requests:
 const createAndSaveUser = async (name, done) =>{
+  // check if the user already exists in our database:
+  const exists = await User.exists({username : name}, (err, data)=>{
+    if(err)return console.log(err);
+    return data;
+  });
+  
+  // if it exists, simply return it:
+  if(exists){
+    const data = User.findOne({username : name}, (err, data)=>{
+      if(err)return console.log(err);
+      // returing data to our callback
+      done(null, data);
+    })
+  }
+  
+  // create user if it does not already exist inside the User object:
   const user = new User({
     username : name
   })
 
+  // save user:
   await user.save((err, data) =>{
     if(err)return console.log(err);
     
+    // again data is passed to callback!
     done(null, data);
   })
 };
+
 
 exports.createAndSaveUser = createAndSaveUser
 
