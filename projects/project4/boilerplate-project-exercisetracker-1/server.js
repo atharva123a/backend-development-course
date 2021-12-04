cf19798814dfe063d63d64fd2e7ded3a42eed1c2
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-12-04 22:44:26 +0530

    server.js now simply handles routes
    Refactored the code so that it no longer deals with objects or database. rather it only deals with routes

--- projects/project4/boilerplate-project-exercisetracker-1/server.js
+++ projects/project4/boilerplate-project-exercisetracker-1/server.js
@@ -5,17 +5,21 @@ require('dotenv').config()
 const router = express.Router();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended : false}))
+// for parsing url data:
+app.use(bodyParser.urlencoded({ extended: false }))
 
 try {
   mongoose = require("mongoose");
-}catch(e){
+} catch (e) {
   console.log(e);
 }
 
+// timeout to check if the callback is made correctly and we just don't keep waiting for the response:
 const TIMEOUT = 10000;
 
+// cors allow FCC or anyone else really access to the url:
 app.use(cors())
+// returns homepage:
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -25,7 +29,7 @@ const User = require("./myApp.js").User;
 
 const createUser = require("./myApp.js").createAndSaveUser;
 
-app.post('/api/users', async(req, res, next) => {
+app.post('/api/users', async (req, res, next) => {
   // this is a timeout function to return timeout done to check if we made a wrong call:
   let t = setTimeout(() => {
     next({ "message": "timeout" })
@@ -33,43 +37,71 @@ app.post('/api/users', async(req, res, next) => {
 
   // the username entered through the form is accessed as:
   const name = req.body.username;
-  
-  const exists = await User.exists({username : name});
 
-  if(exists){
-    const data = await User.findOne({username : name}, (err, data)=>{
-      if(err){
-        return console.log(err);
-      }
-      res.json({
+  // make a call to createUser and return an error or data in required form:
+  await createUser(name, (err, data)=>{
+    if(err)return console.log(err);
+
+    res.json({
       "username" : data.username,
       "_id" : data._id
-    })
     });
-  }
-  
-  // we make a call here to function in app.js and pass value of username to create new user with username equal to this value:
-  const data = createUser(name, (err, data) => {
-    // we made the right call:
-    clearTimeout(t);
-    if (err) {
-      return next(err);
-    }
-    // since user is already created we simply return 
-    // it from our database:
-    User.findById(data._id, (err, user)=>{
-      if(err){
-        next(err);
-      }
+  })
+})
 
-      // returned according to the required format:
-      res.json({
-        "username" : user.username,
-        "_id" : user._id
-      })
-    })
-  });
 
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const router = express.Router();
+const bodyParser = require("body-parser");
+
+// for parsing url data:
+app.use(bodyParser.urlencoded({ extended: false }))
+
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+
+// timeout to check if the callback is made correctly and we just don't keep waiting for the response:
+const TIMEOUT = 10000;
+
+// cors allow FCC or anyone else really access to the url:
+app.use(cors())
+// returns homepage:
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+const User = require("./myApp.js").User;
+
+const createUser = require("./myApp.js").createAndSaveUser;
+
+app.post('/api/users', async (req, res, next) => {
+  // this is a timeout function to return timeout done to check if we made a wrong call:
+  let t = setTimeout(() => {
+    next({ "message": "timeout" })
+  }, TIMEOUT)
+
+  // the username entered through the form is accessed as:
+  const name = req.body.username;
+
+  // make a call to createUser and return an error or data in required form:
+  await createUser(name, (err, data)=>{
+    if(err)return console.log(err);
+
+    res.json({
+      "username" : data.username,
+      "_id" : data._id
+    });
+  })
 })
 
 
