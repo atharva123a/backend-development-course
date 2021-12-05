7714bce783658345a9b88a8db1ddd0702acb5b96
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-12-05 12:23:31 +0530

    myApp.js now can add and save exercise logs

--- projects/project4/boilerplate-project-exercisetracker-1/myApp.js
+++ projects/project4/boilerplate-project-exercisetracker-1/myApp.js
@@ -13,7 +13,13 @@ mongoose.connect(myURI, {
 
 // define userSchema:
 let userSchema = new Schema({
-  username : String
+  username: String,
+  // this is how we declare an array of objects in mongoose:
+  log: [{
+    description: String,
+    duration: Number,
+    date: String
+  }]
 })
 
 // make use of this schema to create User object
@@ -22,38 +28,63 @@ let User = mongoose.model("User", userSchema);
 
 // function to create and save url;
 // we made use of it to handle post requests:
-const createAndSaveUser = async (name, done) =>{
+const createAndSaveUser = async (name, done) => {
   // check if the user already exists in our database:
-  const exists = await User.exists({username : name}, (err, data)=>{
-    if(err)return console.log(err);
+  const exists = await User.exists({ username: name }, (err, data) => {
+    if (err) return console.log(err);
     return data;
   });
-  
+
   // if it exists, simply return it:
-  if(exists){
-    const data = User.findOne({username : name}, (err, data)=>{
-      if(err)return console.log(err);
+  if (exists) {
+    const data = User.findOne({ username: name }, (err, data) => {
+      if (err) return console.log(err);
       // returing data to our callback
       done(null, data);
     })
   }
-  
+
   // create user if it does not already exist inside the User object:
   const user = new User({
-    username : name
+    username: name,
   })
 
   // save user:
-  await user.save((err, data) =>{
-    if(err)return console.log(err);
-    
+  await user.save((err, data) => {
+    if (err) return console.log(err);
+
     // again data is passed to callback!
     done(null, data);
   })
 };
 
+const addAndSaveLog = async (_id, obj, done) => {
+  
+  // get the user using id:
+  let user = await User.findById(_id).exec()
+
+  // push the exercise in the log array 
+  user.log.push(obj)
+  
+  // save exercise to database:
+  await user.save((err, data)=>{
+    if(err)return console.log(err);
+    return data;
+  })
+
+  // return object in the desired format:
+  const returnObj = {
+    "_id": user._id,
+    "username": user.username,
+    "date" : obj.date,
+    "duration" : obj.duration,
+    "description": obj.description
+  }
+  // return to the callback function:
+  done(null, returnObj);
+}
+
 
 exports.createAndSaveUser = createAndSaveUser
-
+exports.addAndSaveLog = addAndSaveLog;
 exports.User = User;
-
