11031753bacdfd0c5db8f9f166ec8a622616c411
Atharva Salokhe <64686082+atharva123a@users.noreply.github.com>
2021-12-06 10:10:04 +0530

    server.js now handles files being posted!

--- projects/project5/boilerplate-project-filemetadata/server.js
+++ projects/project5/boilerplate-project-filemetadata/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
 var app = express();
+//installing mutler package:
+const multer = require("multer");
+//specifying location for submitting files:
+const upload = multer({dest : "myFiles/"});
 
+// for allowing access from other users as well"
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -12,6 +16,16 @@ app.get('/', function (req, res) {
 });
 
 
+// listen for post requests on the following route:
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next)=>{
+  // using multer we are able to acces the info about file which was uploaded!
+  res.json({
+    "name" : req.file.originalname,
+    "type" : req.file.mimetype,
+    "size" : req.file.size
+    })
+  
+})
 
 
 const port = process.env.PORT || 3000;
